e8446a8ff099d77450f28e18c89ccf67617c7c9b
thinhxd12 <thinhxd12@gmail.com>
2024-01-24 11:23:44 +0700

    fixe vite

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -3,7 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 // const supabaseUrl = process.env.VITE_SUPABASE_URL || ''
 // const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || ''
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+
+
+const supabaseUrl = "https://ppedkpvpusslbioqalcl.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwZWRrcHZwdXNzbGJpb3FhbGNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5OTA5MjQsImV4cCI6MjAyMTU2NjkyNH0.0PGp-dB7sw_xnKn-eKhuYMEn1vgMSnCehT1vA8UdzO8"
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
