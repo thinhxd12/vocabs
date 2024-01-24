a49513a9c50802051489e873a0b3120c203296b0
thinhxd12 <thinhxd12@gmail.com>
2024-01-24 11:23:21 +0700

    fixd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-    // start: {
-    //     server: {
-    //         preset: "netlify-builder"
-    //     }
-    // }
+    start: {
+        server: {
+            preset: "netlify"
+        }
+    }
 });
