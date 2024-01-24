50ee726efad5e92738ee418bf990f71a52996166
thinhxd12 <thinhxd12@gmail.com>
2024-01-24 10:34:26 +0700

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
-import 'dotenv/config';
 
-export default defineConfig({});
+export default defineConfig({
+    start: {
+        server: {
+            preset: "netlify_edge"
+        }
+    }
+});
