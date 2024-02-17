06b3cc223f981c1891d31fdf0f2e660e79b5be84
thinhxd12 <thinhxd12@gmail.com>
2024-02-17 19:37:10 +0700

    css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
+import preload from "vite-plugin-preload";
 
 export default defineConfig({
+    plugins: [preload()],
     start: {
         server: {
             prerender: {
