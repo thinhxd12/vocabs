00e1b600ca9e8017bbe44e3bb091d340a7c28c24
thinhxd12 <thinhxd12@gmail.com>
2024-02-23 13:31:08 +0700

    fix26

--- src/functions/netlify-cors/netlify-cors.js
+++ src/functions/netlify-cors/netlify-cors.js
@@ -1,53 +1,19 @@
-const handler = async (event) => {
-  try {
-    switch (event.httpMethod) {
-      case "GET":
-        return handleGet(event);
-      case "POST":
-        return handlePost(event);
-      case "OPTIONS":
-        return handleOptions(event);
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+export default async (req, ctx) => {
+  if (req.method === "POST") {
+    const res = ctx.json({ message: 'you posted!' });
+
+    res.headers.set("Access-Control-Allow-Origin", "*");
+    res.headers.append("Access-Control-Allow-Headers", "*");
+    res.headers.append("Access-Control-Allow-Methods", "*");
+
+    return res;
+  } else if (req.method === "OPTIONS") {
+    const res = new Response();
+
+    res.headers.set("Access-Control-Allow-Origin", "*");
+    res.headers.append("Access-Control-Allow-Headers", "*");
+    res.headers.append("Access-Control-Allow-Methods", "*");
+
+    return res;
   }
 };
-
-function handleGet(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ dummyKey: `DummyGetVal` }),
-    headers: {},
-  };
-}
-
-function handlePost(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ dummyKey: `DummyPostVal` }),
-    headers: {
-      ...getCorsHeaders(),
-    },
-  };
-}
-
-function handleOptions(event) {
-  return {
-    statusCode: 200,
-    headers: {
-      ...getCorsHeaders(),
-    },
-  };
-}
-
-function getCorsHeaders() {
-  return {
-    "access-control-allow-methods": "POST,OPTIONS",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept",
-    "Access-Control-Max-Age": "2592000",
-    "Access-Control-Allow-Credentials": "true",
-  };
-}
-module.exports = { handler };
