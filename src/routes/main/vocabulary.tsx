dc0385869aa284dad37790e797d7662b063d21e3
thinhxd12 <thinhxd12@gmail.com>
2024-02-23 14:09:07 +0700

    fix28

--- src/routes/main/vocabulary.tsx
+++ src/routes/main/vocabulary.tsx
@@ -412,7 +412,7 @@ const Vocabulary: Component<{}> = (props) => {
   // -------------------MOBILE END-------------------- //
 
   const testFunction = async () => {
-    fetch("https://thinhvocabs3.netlify.app/.netlify/functions/hello")
+    fetch("https://thinhvocabs3.netlify.app/.netlify/functions")
       .then((resp) => resp.text())
       .then((data) => {
         // const text = data.last_counted;
