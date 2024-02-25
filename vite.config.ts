86440e44e44ce6402586603e4b5669fb6b611a86
thinhxd12 <thinhxd12@gmail.com>
2024-02-25 13:00:23 +0700

    vite10

--- vite.config.ts
+++ vite.config.ts
@@ -22,10 +22,7 @@ export default defineConfig({
             prerender: {
                 crawlLinks: true
             },
-            preset: "cloudflare",
-            rollupConfig: {
-                external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-            },
+            preset: "cloudflare-pages",
         }
     }
 });
