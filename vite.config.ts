ef036c7204d1b59a1f21ad717e24563180ca386f
thinhxd12 <thinhxd12@gmail.com>
2024-02-25 12:06:22 +0700

    vite6

--- vite.config.ts
+++ vite.config.ts
@@ -19,10 +19,13 @@ export default defineConfig({
     plugins: [preload()],
     start: {
         server: {
-            // prerender: {
-            //     crawlLinks: true
-            // },
+            prerender: {
+                crawlLinks: true
+            },
             preset: "cloudflare-pages",
+            rollupConfig: {
+                external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+            },
         }
     }
 });
