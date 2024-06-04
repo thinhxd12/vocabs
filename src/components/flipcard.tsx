ae858ea95c1225d548d6cb230d4dcaccf2c8198e
thinhxd12 <thinhxd12@gmail.com>
2024-06-04 12:15:50 +0700

    card1215

--- src/components/flipcard.tsx
+++ src/components/flipcard.tsx
@@ -78,34 +78,74 @@ const FlipCard: Component<{}> = (props) => {
       <div class={styles.flipCardLeftContent}>
         <div class={styles.numberFlipContainer}>
           <div class={styles.numberFlipBackground}>
-            <img src="/images/main/flag.jpg" height={186} width={50} />
-          </div>
-          <div class={styles.numberFlipContent}>
-            <Show when={currenText()}>
-              <Show
-                when={currenText().number > 1}
-                fallback={
-                  <div class={styles.flipImage}>
-                    <Motion.div
-                      class={styles.flipImageList}
-                      initial={{ y: -22 }}
-                      animate={{ y: 24, transition: { delay: 3 } }}
-                      transition={{ duration: 0.6, easing: "ease-in-out" }}
-                    >
-                      <img src="/images/main/cup.png" height={44} />
-                      <span class={styles.number}>1</span>
-                    </Motion.div>
-                  </div>
-                }
-              >
-                <div class={styles.numbersContentBackground}>
-                  <div class={styles.numbersContent}>
-                    {renderNumber() >= 100 && (
+            <img src="/images/main/flag.jpg" height={186} width={52} />
+            <div class={styles.numberFlipContent}>
+              <Show when={currenText()}>
+                <Show
+                  when={currenText().number > 1}
+                  fallback={
+                    <div class={styles.flipImage}>
+                      <Motion.div
+                        class={styles.flipImageList}
+                        initial={{ y: -22 }}
+                        animate={{ y: 24, transition: { delay: 3 } }}
+                        transition={{ duration: 0.6, easing: "ease-in-out" }}
+                      >
+                        <img src="/images/main/cup.png" height={44} />
+                        <span class={styles.number}>1</span>
+                      </Motion.div>
+                    </div>
+                  }
+                >
+                  <div class={styles.numbersContentBackground}>
+                    <div class={styles.numbersContent}>
+                      {renderNumber() >= 100 && (
+                        <Motion.div
+                          class={styles.numbers}
+                          animate={{
+                            y: -numbArray()[0] * 46,
+                            width: numbArray()[0] === 1 ? "8px" : "11px",
+                          }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          <div class={styles.number}>0</div>
+                          <div class={styles.number}>1</div>
+                          <div class={styles.number}>2</div>
+                          <div class={styles.number}>3</div>
+                          <div class={styles.number}>4</div>
+                          <div class={styles.number}>5</div>
+                          <div class={styles.number}>6</div>
+                          <div class={styles.number}>7</div>
+                          <div class={styles.number}>8</div>
+                          <div class={styles.number}>9</div>
+                        </Motion.div>
+                      )}
+                      {renderNumber() >= 10 && (
+                        <Motion.div
+                          class={styles.numbers}
+                          animate={{
+                            y: -numbArray()[1] * 46,
+                            width: numbArray()[1] === 1 ? "8px" : "13px",
+                          }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          <div class={styles.number}>0</div>
+                          <div class={styles.number}>1</div>
+                          <div class={styles.number}>2</div>
+                          <div class={styles.number}>3</div>
+                          <div class={styles.number}>4</div>
+                          <div class={styles.number}>5</div>
+                          <div class={styles.number}>6</div>
+                          <div class={styles.number}>7</div>
+                          <div class={styles.number}>8</div>
+                          <div class={styles.number}>9</div>
+                        </Motion.div>
+                      )}
                       <Motion.div
                         class={styles.numbers}
                         animate={{
-                          y: -numbArray()[0] * 46,
-                          width: numbArray()[0] === 1 ? "8px" : "11px",
+                          y: -numbArray()[2] * 46,
+                          width: numbArray()[2] === 1 ? "8px" : "13px",
                         }}
                         transition={{ duration: 0.3 }}
                       >
@@ -120,51 +160,11 @@ const FlipCard: Component<{}> = (props) => {
                         <div class={styles.number}>8</div>
                         <div class={styles.number}>9</div>
                       </Motion.div>
-                    )}
-                    {renderNumber() >= 10 && (
-                      <Motion.div
-                        class={styles.numbers}
-                        animate={{
-                          y: -numbArray()[1] * 46,
-                          width: numbArray()[1] === 1 ? "8px" : "13px",
-                        }}
-                        transition={{ duration: 0.3 }}
-                      >
-                        <div class={styles.number}>0</div>
-                        <div class={styles.number}>1</div>
-                        <div class={styles.number}>2</div>
-                        <div class={styles.number}>3</div>
-                        <div class={styles.number}>4</div>
-                        <div class={styles.number}>5</div>
-                        <div class={styles.number}>6</div>
-                        <div class={styles.number}>7</div>
-                        <div class={styles.number}>8</div>
-                        <div class={styles.number}>9</div>
-                      </Motion.div>
-                    )}
-                    <Motion.div
-                      class={styles.numbers}
-                      animate={{
-                        y: -numbArray()[2] * 46,
-                        width: numbArray()[2] === 1 ? "8px" : "13px",
-                      }}
-                      transition={{ duration: 0.3 }}
-                    >
-                      <div class={styles.number}>0</div>
-                      <div class={styles.number}>1</div>
-                      <div class={styles.number}>2</div>
-                      <div class={styles.number}>3</div>
-                      <div class={styles.number}>4</div>
-                      <div class={styles.number}>5</div>
-                      <div class={styles.number}>6</div>
-                      <div class={styles.number}>7</div>
-                      <div class={styles.number}>8</div>
-                      <div class={styles.number}>9</div>
-                    </Motion.div>
+                    </div>
                   </div>
-                </div>
+                </Show>
               </Show>
-            </Show>
+            </div>
           </div>
         </div>
       </div>
