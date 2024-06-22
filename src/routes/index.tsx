7e6825564823be93b0a934ebf4feaaca5428c8dd
thinhxd12 <thinhxd12@gmail.com>
2024-06-22 13:06:17 +0700

    login106

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,36 +3,43 @@ import { Show } from "solid-js";
 import styles from "./index.module.scss";
 import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { loginAction } from "~/lib";
+import { format } from "date-fns";
 
 export default function Login(props: RouteSectionProps) {
   const loggingIn = useSubmission(loginAction);
+  const today = format(new Date(), "eee").toLowerCase();
 
   return (
     <MetaProvider>
       <Title>login</Title>
       <Meta name="author" content="thinhxd12@gmail.com" />
       <Meta name="description" content="Thinh's Vocabulary Learning App" />
-      <main class={styles.login}>
-        <form action={loginAction} method="post" class={styles.loginForm}>
-          <div class={styles.loginItem}>
+      <main
+        class={styles.login}
+        style={{
+          "background-image": `url("/images/main/${today}.webp")`,
+        }}
+      >
+        <div class={styles.loginContainer}>
+          <form action={loginAction} method="post" class={styles.loginForm}>
             <input name="password" type="password" class={styles.loginInput} />
-          </div>
-          <div class={styles.loginItem}>
-            <button
-              type="submit"
-              class={
-                loggingIn.pending ? styles.loginBtnLoading : styles.loginBtn
-              }
-            >
-              <img src="/images/main/laurel.webp" width={18} />
-            </button>
-          </div>
+            <div class={styles.loginItem}>
+              <button
+                type="submit"
+                class={
+                  loggingIn.pending ? styles.loginBtnLoading : styles.loginBtn
+                }
+              >
+                <img src="/images/main/laurel.webp" width={18} height={18} />
+              </button>
+            </div>
+          </form>
           <Show when={loggingIn.result}>
             <p class={styles.loginAlert} role="alert" id="error-message">
               {loggingIn.result!.message}
             </p>
           </Show>
-        </form>
+        </div>
       </main>
     </MetaProvider>
   );
