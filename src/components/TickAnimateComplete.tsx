f611c1480f6bd3c730870c7d67da800b8c3ca232
thinhxd12 <thinhxd12@gmail.com>
2024-07-15 19:27:13 +0700

    css727

--- src/components/TickAnimateComplete.tsx
+++ src/components/TickAnimateComplete.tsx
@@ -38,7 +38,7 @@ const TickAnimateComplete: Component<{
         >
           <span class="tick-flip-panel-front-text">
             <span class="tick-flip-panel-text-wrapper">
-              <img src="/images/main/cup.webp" width={52} />
+              <img src="/images/main/cup.webp" width={110} />
             </span>
           </span>
           <span class="tick-flip-panel-front-shadow" style="opacity: 0;"></span>
@@ -98,7 +98,7 @@ const TickAnimateComplete: Component<{
         >
           <span class="tick-flip-panel-back-text">
             <span class="tick-flip-panel-text-wrapper">
-              <img src="/images/main/cup.webp" width={52} />
+              <img src="/images/main/cup.webp" width={110} />
             </span>
           </span>
           <Motion.span
