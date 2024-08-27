975565b8168308d65007883fab36fcbacafa7d75
thinhxd12 <thinhxd12@gmail.com>
2024-08-27 22:52:06 +0700

    css1052

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://ffmpeg-4sn.pages.dev/styles.css"></link>
           {assets}
         </head>
         <body>
