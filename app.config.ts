f1154987a5b133987828a431c78794843afcc496
thinhxd12 <thinhxd12@gmail.com>
2024-12-09 07:44:56 +0700

    change744

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@solidjs/start/config";
-import deno from "@deno/vite-plugin";
 
 export default defineConfig({
   server: {
@@ -9,12 +8,9 @@ export default defineConfig({
     preset: "vercel",
     compatibilityDate: "2024-11-18",
   },
-  solid: {
-    babel: {
-      compact: true,
-    },
-  },
-  vite: {
-    plugins: [deno()],
-  },
+  // solid: {
+  //   babel: {
+  //     compact: true,
+  //   },
+  // },
 });
