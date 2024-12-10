ad16f766fcd521efc15fe3a4f70810a20090f494
thinhxd12 <thinhxd12@gmail.com>
2024-12-10 14:02:56 +0700

    fix202

--- src/components/FlipNumber.tsx
+++ src/components/FlipNumber.tsx
@@ -10,7 +10,6 @@ import TickStatic from "./TickStatic";
 import TickAnimate from "./TickAnimate";
 import TickAnimateComplete from "./TickAnimateComplete";
 import { Motion, Presence } from "solid-motionone";
-import { easeOutBounce } from "~/lib/utils";
 
 const FlipNumber: Component<{
   value: number;
@@ -26,6 +25,21 @@ const FlipNumber: Component<{
       setShowFlipAnimate(false);
     }, 3600);
   });
+
+  function easeOutBounce(x: number): number {
+    const n1 = 7.5625;
+    const d1 = 2.75;
+
+    if (x < 1 / d1) {
+      return n1 * x * x;
+    } else if (x < 2 / d1) {
+      return n1 * (x -= 1.5 / d1) * x + 0.75;
+    } else if (x < 2.5 / d1) {
+      return n1 * (x -= 2.25 / d1) * x + 0.9375;
+    } else {
+      return n1 * (x -= 2.625 / d1) * x + 0.984375;
+    }
+  }
 
   return (
     <Presence>
